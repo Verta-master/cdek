d3e3e6e3cb551b82210955a2a978c207c21cf37e
Verta-master <vertyagina@gmail.com>
2018-01-17 22:04:30 +0600

    Update 2018-01-17T16:04:25.142Z

--- js/script.js
+++ js/script.js
@@ -2,15 +2,20 @@
 var navMain = document.querySelector(".menu");
 var navToggle = document.querySelector(".menu__btn");
 
-navToggle.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  if (navMain.classList.contains("menu--closed")) {
-    navMain.classList.remove("menu--closed");
-    navMain.classList.add("menu--opened");
-  } else {
-    navMain.classList.add("menu--closed");
-    navMain.classList.remove("menu--opened");
-  }
+//navToggle.addEventListener("click", function(evt) {
+//  evt.preventDefault();
+//  if (navMain.classList.contains("menu--closed")) {
+//    navMain.classList.remove("menu--closed");
+//    navMain.classList.add("menu--opened");
+//  } else {
+//    navMain.classList.add("menu--closed");
+//    navMain.classList.remove("menu--opened");
+//  }
+//});
+
+$('.menu__btn').click(function() {
+  $(this).next().slideToggle();
+  $('.menu__wrap::after', $(this)).slideToggle(1000);
 });
 
 //Scroll to menu anchor
